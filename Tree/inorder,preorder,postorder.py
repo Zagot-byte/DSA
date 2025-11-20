8ed4a6ab5f5254fca2a51cf8069ade096ba28562
Zagot-byte <sriraghav006@gmail.com>
2025-11-21 01:49:54 +0530

    Refactor tree traversal functions for printing
    Updated traversal functions to print results instead of returning lists.

--- Tree/inorder,preorder,postorder.py
+++ Tree/inorder,preorder,postorder.py
@@ -4,22 +4,31 @@ class Node:
         self.right = None
         self.val = key
 
-def inorder_traversal(root):
-    return inorder_traversal(root.left) + [root.val] + inorder_traversal(root.right) if root else []
+def inorder(root):
+    if root:
+        inorder(root.left)
+        print(root.val, end=' ')
+        inorder(root.right)
 
-def preorder_traversal(root):
-    return [root.val] + preorder_traversal(root.left) + preorder_traversal(root.right) if root else []
+def preorder(root):
+    if root:
+        print(root.val, end=' ')
+        preorder(root.left)
+        preorder(root.right)
 
-def postorder_traversal(root):
-    return postorder_traversal(root.left) + postorder_traversal(root.right) + [root.val] if root else []
+def postorder(root):
+    if root:
+        postorder(root.left)
+        postorder(root.right)
+        print(root.val, end=' ')
 
 # Example usage:
-# Constructing a binary tree:
-#       1
-#      / \
-#     2   3
-#    / \
-#   4   5
+# Constructing this binary tree:
+#        1
+#      /   \
+#     2     3
+#    / \     
+#   4   5    
 
 root = Node(1)
 root.left = Node(2)
@@ -27,8 +36,10 @@ root.right = Node(3)
 root.left.left = Node(4)
 root.left.right = Node(5)
 
-inorder_result = inorder_traversal(root)
-preorder_result = preorder_traversal(root)
-postorder_result = postorder_traversal(root)
-
-inorder_result, preorder_result, postorder_result
+print("Inorder traversal:")
+inorder(root)
+print("\nPreorder traversal:")
+preorder(root)
+print("\nPostorder traversal:")
+postorder(root)
+print()
